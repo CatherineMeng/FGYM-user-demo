ef23413f93bfa6330802672a7fb005a657e66f1a
CatherineMeng <myuan2009@gmail.com>
2021-05-05 13:44:26 -0700

    Added cartpole example

---
+++ cartpole_example/cartpole.py
@@ -0,0 +1,208 @@
+import matplotlib.pyplot as plt
+import random
+import torch
+import torch.nn as nn
+import copy
+from collections import deque
+from torch import randint
+import gym
+from tqdm import tqdm
+
+# ==========================================================
+# ===================RANDOM AGENT ==========================
+# ==========================================================
+# rew_arr = []
+# episode_count = 100
+# env = gym.make('CartPole-v0')
+# for i in range(episode_count):
+#     obs, done, rew = env.reset(), False, 0
+#     while (done != True) :
+#         A =  randint(0,env.action_space.n,(1,))
+#         obs, reward, done, info = env.step(A.item())
+#         rew += reward
+#     rew_arr.append(rew)
+    
+# print("average reward per episode :",sum(rew_arr)/ len(rew_arr))
+# ==========================================================
+# ===================RANDOM AGENT END=======================
+# ==========================================================
+
+
+# ==========================================================
+# ====================== DQN CLASS =========================
+# ==========================================================
+class DQN_Agent:
+    
+    def __init__(self, seed, layer_sizes, lr, sync_freq, exp_replay_size):
+        torch.manual_seed(seed)
+        self.q_net = self.build_nn(layer_sizes)
+        self.target_net = copy.deepcopy(self.q_net)
+        # self.q_net.cuda()
+        # self.target_net.cuda()
+        self.loss_fn = torch.nn.MSELoss()
+        self.optimizer = torch.optim.Adam(self.q_net.parameters(), lr=lr)
+        
+        self.network_sync_freq = sync_freq
+        self.network_sync_counter = 0
+        self.gamma = torch.tensor(0.95).float()
+        # self.gamma = torch.tensor(0.95).float().cuda()
+        self.experience_replay = deque(maxlen = exp_replay_size)  
+        return
+        
+        # changed Tanh to Relu, bias to False
+    def build_nn(self, layer_sizes):
+        assert len(layer_sizes) > 1
+        layers = []
+        for index in range(len(layer_sizes)-1):
+            linear = nn.Linear(layer_sizes[index], layer_sizes[index+1],bias=False)
+            act =    nn.ReLU() if index < len(layer_sizes)-2 else nn.Identity()
+            # act =    nn.Tanh() if index < len(layer_sizes)-2 else nn.Identity()
+            layers += (linear,act)
+        return nn.Sequential(*layers)
+    
+    # ======================New helper function======================
+    # def get_params(self):
+    #     list_of_params=[[] for _ in range(len(layer_sizes))]
+    #     for 
+    #     return list(self.q_net.parameters())
+    # ====================End New helper function====================
+    def get_action(self, state, action_space_len, epsilon):
+        # We do not require gradient at this point, because this function will be used either
+        # during experience collection or during inference
+        with torch.no_grad():
+            Qp = self.q_net(torch.from_numpy(state).float())
+            # Qp = self.q_net(torch.from_numpy(state).float().cuda())
+        Q,A = torch.max(Qp, axis=0)
+        A = A if torch.rand(1,).item() > epsilon else torch.randint(0,action_space_len,(1,))
+        return A
+    
+    def get_q_next(self, state):
+        with torch.no_grad():
+            qp = self.target_net(state)
+        q,_ = torch.max(qp, axis=1)    
+        return q
+    
+    def collect_experience(self, experience):
+        self.experience_replay.append(experience)
+        return
+    
+    def sample_from_experience(self, sample_size):
+        if(len(self.experience_replay) < sample_size):
+            sample_size = len(self.experience_replay)   
+        sample = random.sample(self.experience_replay, sample_size)
+        s = torch.tensor([exp[0] for exp in sample]).float()
+        a = torch.tensor([exp[1] for exp in sample]).float()
+        rn = torch.tensor([exp[2] for exp in sample]).float()
+        sn = torch.tensor([exp[3] for exp in sample]).float()   
+        return s, a, rn, sn
+    
+    def train(self, batch_size ):
+        s, a, rn, sn = self.sample_from_experience( sample_size = batch_size)
+        if(self.network_sync_counter == self.network_sync_freq):
+            self.target_net.load_state_dict(self.q_net.state_dict())
+            self.network_sync_counter = 0
+        
+        # predict expected return of current state using main network
+        qp = self.q_net(s)
+        # qp = self.q_net(s.cuda())
+        pred_return, _ = torch.max(qp, axis=1)
+        
+        # get target return using target network
+        q_next = self.get_q_next(sn)
+        target_return = rn + self.gamma * q_next
+        # q_next = self.get_q_next(sn.cuda())
+        # target_return = rn.cuda() + self.gamma * q_next
+        
+        loss = self.loss_fn(pred_return, target_return)
+        self.optimizer.zero_grad()
+        loss.backward(retain_graph=True)
+        self.optimizer.step()
+        
+        self.network_sync_counter += 1       
+        return loss.item()
+# ==========================================================
+# ====================== DQN CLASS END======================
+# ==========================================================
+
+
+env = gym.make('CartPole-v0')
+input_dim = env.observation_space.shape[0]
+output_dim = env.action_space.n
+exp_replay_size = 256
+# original seed 1423
+agent = DQN_Agent(seed = 0, layer_sizes = [input_dim, 64, output_dim], lr = 1e-3, sync_freq = 5, exp_replay_size = exp_replay_size)
+
+# initiliaze experiance replay      
+index = 0
+for i in range(exp_replay_size):
+    obs = env.reset()
+    done = False
+    while(done != True):
+        A = agent.get_action(obs, env.action_space.n, epsilon=1)
+        obs_next, reward, done, _ = env.step(A.item())
+        agent.collect_experience([obs, A.item(), reward, obs_next])
+        obs = obs_next
+        index += 1
+        if( index > exp_replay_size ):
+            break
+            
+# Main training loop
+losses_list, reward_list, episode_len_list, epsilon_list  = [], [], [], []
+index = 128
+episodes = 10000
+epsilon = 1
+
+for i in tqdm(range(episodes)):
+    obs, done, losses, ep_len, rew = env.reset(), False, 0, 0, 0
+    while(done != True):
+        ep_len += 1 
+        A = agent.get_action(obs, env.action_space.n, epsilon)
+        obs_next, reward, done, _ = env.step(A.item())
+        agent.collect_experience([obs, A.item(), reward, obs_next])
+       
+        obs = obs_next
+        rew  += reward
+        index += 1
+        
+        if(index > 128):
+            index = 0
+            for j in range(4):
+                loss = agent.train(batch_size=16)
+                losses += loss      
+    if epsilon > 0.05 :
+        epsilon -= (1 / 5000)
+    
+    losses_list.append(losses/ep_len), reward_list.append(rew), episode_len_list.append(ep_len), epsilon_list.append(epsilon)
+
+# ================================
+# ========check params============
+# ================================
+# agent_param = list(agent.parameters())
+for i in range(len(agent.q_net)):
+    if (i%2==0):
+        print("layer",i,"shape:",agent.q_net[i].weight.size())
+        print(agent.q_net[i].weight)
+# ================================
+# Following used to plot reward
+# ================================
+list_episodes = [i for i in range(episodes)]
+  
+plt.plot(list_episodes, reward_list)
+plt.title('Reward Vs Episodes')
+plt.xlabel('Episodes')
+plt.ylabel('Reward')
+plt.show()
+# ================================
+# Following used to generate video
+# ================================
+# env = gym.make('CartPole-v0')
+# env = gym.wrappers.Monitor(env, "record_dir",force=True)
+# for i in tqdm(range(2)):
+#     obs, done, rew = env.reset(), False, 0
+#     while (done != True) :
+#         A =  agent.get_action(obs, env.action_space.n, epsilon = 0)
+#         obs, reward, done, info = env.step(A.item())
+#         rew += reward
+#         sleep(0.01)
+#         env.render()  
+#     print("episode : {}, reward : {}".format(i,rew)) 
