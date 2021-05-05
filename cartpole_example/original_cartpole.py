552e0cf029d691f3609c74776ebe2a3d3661bd1e
CatherineMeng <myuan2009@gmail.com>
2021-05-05 13:49:18 -0700

    Added cartpole example

--- cartpole_example/original_cartpole.py
+++ cartpole_example/original_cartpole.py
@@ -145,3 +145,19 @@ plt.title('Reward Vs Episodes')
 plt.xlabel('Episodes')
 plt.ylabel('Reward')
 plt.show()
+
+
+# ================================
+# Inference phase (with video)
+# ================================
+# env = gym.make('CartPole-v0')
+# env = gym.wrappers.Monitor(env, "record_dir",force=True)
+# for i in tqdm(range(2)):
+#     obs, done, rew = env.reset(), False, 0
+#     while (done != True) :
+#         A =  agent.get_action(obs, env.action_space.n, epsilon = 0)
+#         obs, reward, done, info = env.step(A.item())
+#         rew += reward
+#         sleep(0.01)
+#         env.render()  
+#     print("episode : {}, reward : {}".format(i,rew))
